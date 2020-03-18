627e9cc1dde88a027246e7bc02d4c05fc3c67974
AnnaCape <61944693+AnnaCape@users.noreply.github.com>
2020-03-18 00:38:28 -0300

    Update main.c
    Tentei do fazer os test, en modo manual estava certo mas com o make não.

--- src/main.c
+++ src/main.c
@@ -10,37 +10,49 @@ int main()
 	///initialisation
 	char buffer[tam_buffer];
 	char rev[tam_buffer];
+	char res[tam_buffer];
 	int espace[tam_buffer];
 	for(int g = 0; espace[g] != '\0'; g++) {
 			espace[g]=0;
 	}
     int i, j, b;
-    int count =0;
+    int count =1;
     j =-1;
-	int q=1;
-	espace[0]=0;
 	b=0;
 	///
 	fgets(buffer, tam_buffer, stdin);
 	//fin de mot
-	for(int h = 0; buffer[h] != '\0'; h++) {
-		if(buffer[h] == ' ' || buffer[h] == '\n'){
-			espace[q++]=h; 
+	for(int h =0; buffer[h] != '\0'; h++) {
+		if(buffer[h] == ' ' || buffer[h] == '\t' || buffer[h] == '\n'){
+			espace[count]=h; 
 			count++;
 		}
 	}
-//Inversion
-	while( b<=count){	
-		for (i = espace[b+1]; i>=espace[b]; i--)
+	
+	while( b<count){	
+		for (i = espace[b+1]; i >=espace[b]; i--)
 		{
 			if(buffer[i]=='\n'){
+				rev[j++]=' ';
 				i--;
 			}
 			rev[j++] = buffer[i];
 		}
-    rev[j] = '\0';
+    rev[j] ='\0';
 	b++;
- }
-    printf(rev);
+		}	
+ //pb espacio #complicado demais
+	int r=0;
+	int s=0;
+ while (rev[r] != '\0')
+   {
+      if ((rev[r] == ' ' && rev[r+1] == ' ') != 1) {
+        res[s] = rev[r];
+        s++;
+      }
+      r++;
+   }
+   res[s] = '\0';
+	printf("%s\n", res);
     return 0;
 }
